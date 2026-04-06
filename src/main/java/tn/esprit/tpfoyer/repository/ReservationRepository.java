aefbe93dce1961789665bb9d555649e1e4dbc617
Mohamed Dhia Chaouachi <mohameddhia.chaouachi@esprit.tn>
2026-04-06 15:35:30 +0100

    jpql

--- src/main/java/tn/esprit/tpfoyer/repository/ReservationRepository.java
+++ src/main/java/tn/esprit/tpfoyer/repository/ReservationRepository.java
@@ -1,7 +1,33 @@
 package tn.esprit.tpfoyer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import tn.esprit.tpfoyer.entity.Etudiant;
 import tn.esprit.tpfoyer.entity.Reservation;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, String> {
+
+    @Query("SELECT r FROM Reservation r WHERE :etudiant MEMBER OF r.etudiantList")
+    List<Reservation> findReservationsByEtudiant(@Param("etudiant") Etudiant etudiant);
+
+    @Query("""
+	    SELECT r
+	    FROM Reservation r
+	    JOIN r.chambre c
+	    JOIN c.bloc b
+	    WHERE b.nomBloc = :nomBloc
+	    """)
+    List<Reservation> findReservationsByNomBloc(@Param("nomBloc") String nomBloc);
+
+    @Query("""
+	    SELECT c.numeroChambre, COUNT(r)
+	    FROM Reservation r
+	    JOIN r.chambre c
+	    GROUP BY c.numeroChambre
+	    HAVING COUNT(r) >= :minReservations
+	    """)
+    List<Object[]> countReservationsByChambre(@Param("minReservations") Long minReservations);
 }
